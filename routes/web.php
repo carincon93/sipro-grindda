c718cf9ad36361136ac81b46e2d81f94d67eb2fc
Cristian Rincón <carincon93@gmail.com>
2020-12-18 17:07:18 -0500

    Error fixed

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ use App\Http\Controllers\ActividadController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::middleware(['auth'])->group(function () {
